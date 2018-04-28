13b13079a4685db93488efe3f2adeb0644604fb4
upaseneha <upaseneha@gmail.com>
2018-04-28 18:13:27 +0530

    Added register component

--- actionCreation/src/app/app.module.ts
+++ actionCreation/src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
+const appRoutes:Routes=[
+  {
+    path : 'register',
+    component : RegisterComponent
+  },
+
+  {
+    path:'',
+    component : LoginComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
